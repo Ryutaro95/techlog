bbe19a63623ef074875f1e8097d0d9447ba40227
Ryutaro95 <ryutaro9595@gmail.com>
2019-10-13 00:56:26 +0900

    sign_in/sign_out/sign_up画面への遷移確認のため仮のTOPページを作成 ユーザーモデルのバリデーションに関するテスト作成＆GREEN

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  it "FactoryBotのユーザーが有効な状態であること" do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+
   it "ユーザ名、メールアドレス、パスワードがあれば有効であること" do
     user = FactoryBot.build(:user)
     expect(user).to be_valid
@@ -31,25 +36,37 @@ RSpec.describe User, type: :model do
   it "重複したメールアドレスなら無効な状態であること" do
     FactoryBot.create(:user, email: "test@user.com")
     user = FactoryBot.build(:user, email: "test@user.com")
-    puts user.valid?  
+    user.valid?
 
-    expect(user.errors[:name]).to include("has already been taken")
+    expect(user.errors[:email]).to include("has already been taken")
   end
 
 
   context "パスワードが6文字のとき" do
-    it "有効な状態であること"
+    it "有効な状態であること" do
+      user = FactoryBot.build(:user, password: "a" * 6)
+      expect(user).to be_valid
+    end
   end
 
   context "パスワードが5文字のとき" do
-    it "無効な状態であること"
+    it "無効な状態であること" do
+      user = FactoryBot.build(:user, password: "a" * 5)
+      expect(user).to_not be_valid
+    end
   end
 
   context "パスワードが20文字のとき" do
-    it "有効な状態であること"
+    it "有効な状態であること" do
+      user = FactoryBot.build(:user, password: "a" * 20)
+      expect(user).to be_valid
+    end
   end
 
   context "パスワードが21文字のとき" do
-    it "無効な状態であること"
+    it "無効な状態であること" do
+      user = FactoryBot.build(:user, password: "a" * 21)
+      expect(user).to_not be_valid
+    end
   end
 end
