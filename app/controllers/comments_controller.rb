a1d6469cef4c822f2982f4997bab8c6ea13a5425
Ryutaro95 <ryutaro9595@gmail.com>
2019-11-10 03:57:11 +0900

    ブログ記事とユーザーに紐づくコメントの投稿/削除機能をAjaxで実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+class CommentsController < ApplicationController
+
+  def create
+    user = current_user
+    @comment = user.comments.build(comment_params)
+    @comment.post_id = params[:post_id]
+    @comments = Comment.where(post_id: @comment.post_id).page(params[:page]).per(10).order(created_at: :desc)
+    respond_to do |format|
+      if @result = @comment.save
+        format.html
+        format.js
+      else
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @comments = Comment.where(post_id: @comment.post_id).page(params[:page]).per(10).order(created_at: :desc)
+    respond_to do |format|
+      if @comment.destroy
+        format.js
+      else
+        format.js
+      end
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:post_id, :comment)
+    end
+end
