e179f6193aec05e83cd2dc8e2162850bda13fa4d
Ryutaro95 <ryutaro9595@gmail.com>
2020-01-06 21:57:02 +0900

    いいね機能/フォロー機能の統合テストを追加

--- spec/features/likes_spec.rb
+++ spec/features/likes_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.feature "Likes", type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+  let(:other_user) { FactoryBot.create(:user, name: "その他のユーザー") }
+  let!(:post) { FactoryBot.create(:post, user: other_user) }
 
+  describe "ブログ記事をいいね" do
+    scenario "他のユーザーが投稿したブログ記事をいいねできる", js: true do
+      sign_in_as(user)
+      click_link "ブログ記事タイトル"
+
+      expect{
+        within ".like-button" do
+          find(".like-btn").click
+        end
+        expect(page).to have_css ".unlike-btn"
+      }.to change(Like, :count).by(1)
+    end
+  end
+
+  describe "ブログ記事のいいね解除" do
+    scenario "いいねした記事をいいね解除する", js: true do
+      sign_in_as(user)
+        click_link "ブログ記事タイトル"
+        within ".like-button" do
+          find(".like-btn").click
+        end
+        sleep 3
+  
+        expect{
+          within ".like-button" do
+            find(".unlike-btn").click
+          end
+          expect(page).to have_css ".like-btn"
+        }.to change(Like, :count).by(-1)
+    end
+  end
 end
--- spec/features/user_follow_relations_spec.rb
+++ spec/features/user_follow_relations_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.feature "UserFollowRelations", type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+  let(:other_user) { FactoryBot.create(:user, name: "その他のユーザー") }
+  let!(:post) { FactoryBot.create(:post, user: other_user) }
 
+  describe "ユーザーフォロー" do
+    scenario "その他のユーザーをフォローできる", js: true do
+      sign_in_as(user)
+      click_link "その他のユーザー"
+
+      expect{
+        click_on "フォロー"
+
+        expect(page).to have_button "フォロー解除"
+        expect(page).to have_content "ユーザーをフォローしました"
+      }.to change(UserFollowRelation, :count).by(1)
+    end
+  end
+
+  describe "ユーザーフォロー解除" do
+    scenario "その他のユーザーをフォロー解除できる", js: true do
+      sign_in_as(user)
+      click_link "その他のユーザー"
+      click_on "フォロー"
+      
+      expect{
+        click_on "フォロー解除"
+        expect(page).to have_button "フォロー"
+        expect(page).to have_content "ユーザーのフォローを解除しました"
+      }.to change(UserFollowRelation, :count).by(-1)
+    end
+  end
 end
