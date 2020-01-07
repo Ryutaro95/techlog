4636a3a3e6b45f06d14f00f0e504b0fd2f1e049e
Ryutaro95 <ryutaro9595@gmail.com>
2020-01-07 19:01:45 +0900

    Userテーブルに対応するidがない場合、例外が発生するように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
     @posts = Post.where(user_id: @user.id).page(params[:page]).per(10).order(updated_at: :desc)
     @following_posts = @user.following_posts.page(params[:page]).per(10).order(updated_at: :desc)
   end
