25c24344c187b7da820b1a29cbdaf4dcefb29812
Ryutaro95 <ryutaro9595@gmail.com>
2020-01-09 00:13:55 +0900

    マークダウン表記の言語によるシンタックスハイライトで予期しない言語が入力された際に発生していたエラーを修正

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,36 +1,3 @@
-# # gem 'redcarpet'用
-# module MarkdownHelper
-#   def markdown(text)
-#     html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
-#     unless @markdown
-#       options = {
-#         filter_html: true,
-#         autolink: true,
-#         space_after_headers: true,
-#         no_intra_emphasis: true,
-#         fenced_code_blocks: true,
-#         tables: true,
-#         hard_wrap: true,
-#         xhtml: true,
-#         lax_html_blocks: true,
-#         strikethrough: true
-#       }
-#       renderer = Redcarpet::Render::HTML.new(options)
-#       @markdown = Redcarpet::Markdown.new(renderer)
-#     end
-
-#     html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
-#     markdown = Redcarpet::Markdown.new(html_render, REDCARPET_OPTIONS)
-#     @markdown.render(text).html_safe
-#   end
-
-#   class HTMLwithCoderay < Redcarpet::Render::HomeController
-#     def block_code(code, language)
-#       CodeRay.scan(code, language || 'md').div
-#     end
-#   end
-# end
-
 module MarkdownHelper
   REDCARPET_OPTIONS = {
     filter_html: true,
@@ -53,7 +20,18 @@ module MarkdownHelper
 
   class HTMLwithCoderay < Redcarpet::Render::HTML
     def block_code(code, language)
-      CodeRay.scan(code, language || 'md').div
+      case language.to_s
+      when /^(?=.*rb).*$/
+        lang = 'ruby'
+      when /^(?=.*yml).*$/
+        lang = 'yaml'
+      when /^(?=.*md).*$/
+        lang = 'md'
+      else
+        lang = 'nothing'
+      end
+
+      CodeRay.scan(code, lang).div
     end
   end
 end
