a44b31cf4a4e5fe441069c33b11ad8845efa48fc
Ryutaro95 <ryutaro9595@gmail.com>
2020-01-29 08:52:07 +0900

    タグ機能実装のため、Tagモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_142429) do
+ActiveRecord::Schema.define(version: 2020_01_28_233721) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -71,6 +71,13 @@ ActiveRecord::Schema.define(version: 2019_12_05_142429) do
     t.index ["post_id"], name: "index_stocks_on_post_id"
     t.index ["user_id", "post_id"], name: "index_stocks_on_user_id_and_post_id", unique: true
     t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "user_follow_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
