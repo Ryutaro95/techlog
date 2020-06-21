71040482b202298a7fa33fb9afabcf079342deb3
Ryutaro95 <ryutaro9595@gmail.com>
2020-06-22 05:43:52 +0900

    usersカラムに管理者権限を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
-20.times.each do |i|
-  User.create!(
-    name: "テストユーザー#{i + 1}",
-    email: "test.#{i + 1}@user.com",
-    profile: "はじめまして、テストユーザー#{i + 1}です。\nよろしくお願いします",
-    password: "password"
-  )
-end
+# 20.times.each do |i|
+#   User.create!(
+#     name: "テストユーザー#{i + 1}",
+#     email: "test.#{i + 1}@user.com",
+#     profile: "はじめまして、テストユーザー#{i + 1}です。\nよろしくお願いします",
+#     password: "password"
+#   )
+# end
+
+User.create!(
+   name: "かんたんユーザー",
+   email: "kantan@user.com",
+   profile: "かんたんユーザーです。TECHLOGの管理者です。",
+   password: "password",
+   admin: true
+)
